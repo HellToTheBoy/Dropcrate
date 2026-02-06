5862a1b49f25557a854ac604ce35c7f3fac3cb9c
HellToTheBoy <hellcurran@gmail.com>
2026-02-06 23:24:39 +0530

    Update route.js

--- app/api/logout/route.js
+++ app/api/logout/route.js
@@ -2,8 +2,8 @@ export async function GET() {
   return new Response(null, {
     status: 302,
     headers: {
-      Location: "/",
       "Set-Cookie": "steamId=; Path=/; Max-Age=0",
+      Location: "/",
     },
   });
 }
